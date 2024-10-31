284f6c2a257065ae259647aff17fef6b8e307732
Khanh <ngophuckhanh@gmail.com>
2024-11-01 01:18:24 +0700

    addPreviuosAndNextBTN

--- pages/products/homework/index.js
+++ pages/products/homework/index.js
@@ -1,5 +1,9 @@
+
+// CẦN CHO BIẾN CURRENTPAGE NÀY MẶC ĐỊNH Ở PAGE 1
+let currentPage = 1;
+// CẦN BỎ PRODUCTS VÀO TRONG HÀM ĐỂ DỄ DÀNG HIỂN THỊ
 function renderProducts(products){
-    document.getElementById("product-list").textContent = ''
+    document.getElementById("products-list").textContent = ''
     for(let i = 0; i < products.length;i++){
         const productElement = document.createElement("li")
         const productIdElement = document.createElement("p")
@@ -7,40 +11,63 @@ function renderProducts(products){
         const productCategoryElement = document.createElement("p")
         const productPriceElement = document.createElement("p")
         const productRatingElement = document.createElement("p")
+        const productPictureElement = document.createElement("img")
         
         productIdElement.textContent = products[i].id
         productNameElement.textContent = products[i].name
         productCategoryElement.textContent = products[i].category
         productPriceElement.textContent = products[i].price
         productRatingElement.textContent = products[i].rating
+        productPictureElement.src = products[i].imgurl  
     
         productElement.appendChild(productIdElement)
         productElement.appendChild(productNameElement)
         productElement.appendChild(productCategoryElement)
         productElement.appendChild(productPriceElement)
         productElement.appendChild(productRatingElement)
+        productElement.appendChild(productPictureElement)
+
 
         document.getElementById("products-list").appendChild(productElement)
     }
 }
 
-
-async function fetchProducts() {
+// HÀM GỌI DỮ LIỆU TỪ API 
+async function fetchProducts(page) {
     try{
-        const response = await fetch("https://form-test-api.vercel.app/api/products?page=1")
+        const response = await fetch(`https://form-test-api.vercel.app/api/products?page=${page}`);
         const data = await response.json()
         return data
     }catch(error){
         console.log(error)
     }
 }
-
+// HÀM MAIN CHÍNH CŨNG LÀ HÀM CẬP NHẬT CURRENT PAGE
 async function main() {
-    const data = await fetchProducts()
+    const data = await fetchProducts(currentPage)
     const products = data.products
-    renderProducts(products)
+    if (data) {
+        renderProducts(products)
+        document.getElementById("pageNumber").textContent = `Trang ${data.currentPage} / ${data.totalPages}`
+    }
 }
 
 
+document.getElementById("nextPagebtn").addEventListener("click", () => {
+    if(currentPage < 4){
+        currentPage++
+        main()
+    }
+});
+
+document.getElementById("previousPagebtn").addEventListener("click", () => {
+    if (currentPage > 1) {
+        currentPage--
+        main()
+    }
+})
+
 main()
+
+
 
